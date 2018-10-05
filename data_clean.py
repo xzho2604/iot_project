d187c3d4ec62c996022327449eeb6533f7dc9fdf
xb2342996 <536523401@qq.com>
2018-10-05 13:51:58 +1000

    update file input

--- data_clean.py
+++ data_clean.py
@@ -1,51 +1,68 @@
 import numpy as np
-
-group_list = []
-data_dict = {}
-cord_list = []
+import sys
+import os
 
 
-count = 0
-with open("record02.txt", 'r') as f:
-    for l in f.readlines():
-        tag, _, rssi = l.strip('\n').split(' ')
-        if tag != '-----End':
-            if tag not in data_dict:
-                data_dict[tag] = [int(rssi)]
+def main(input, output):
+
+    group_list = []
+    data_dict = {}
+    cord_list = []
+    count = 0
+
+    with open(input, 'r') as f:
+        for l in f.readlines():
+            tag, _, rssi = l.strip('\n').split(' ')
+            if tag != '-----End':
+                if tag not in data_dict:
+                    data_dict[tag] = [int(rssi)]
+                else:
+                    data_dict[tag].append(int(rssi))
             else:
-                data_dict[tag].append(int(rssi))
-        else:
-            count += 1
-            if count == 2:
-                _, position = rssi.split('-----')
-                x, y = position.split(',')
-                cord_list.append((x, y))
-                group_list.append(data_dict)
-                data_dict = {}
-                count = 0
+                count += 1
+                if count == 2:
+                    _, position = rssi.split('-----')
+                    x, y = position.split(',')
+                    cord_list.append((x, y))
+                    group_list.append(data_dict)
+                    data_dict = {}
+                    count = 0
 
 
-
-for i in range(len(group_list)):
-    for key, value in group_list[i].items():
-        group_list[i][key] = np.mean(value)
-
-    print(group_list[i])
-    print(cord_list[i])
-
-with open('clean_record.txt', 'a') as f:
-    f.write('%6d %6d %6d %6d %6d %6s %6s\n' % (1,2,3,4,5,'x','y'))
 
     for i in range(len(group_list)):
-        for j in range(1, 6):
-            if str(j) not in group_list[i]:
-                f.write('%6.2f ' % 0)
-            else:
-                f.write('%6.2f ' % group_list[i][str(j)])
-        f.write('%6s %6s' % (cord_list[i][0], cord_list[i][1]))
-        f.write('\n')
+        for key, value in group_list[i].items():
+            group_list[i][key] = np.mean(value)
+
+        # print(group_list[i])
+        # print(cord_list[i])
+
+    if os.path.exists(output):
+        os.remove(output)
+
+    with open(output, 'a') as f:
+        f.write('%d %d %d %d %d %s %s\n' % (1,2,3,4,5,'x','y'))
+
+        for i in range(len(group_list)):
+            for j in range(1, 6):
+                if str(j) not in group_list[i]:
+                    f.write('%.2f ' % 0)
+                else:
+                    f.write('%.2f ' % group_list[i][str(j)])
+            f.write('%s %s' % (cord_list[i][0], cord_list[i][1]))
+            f.write('\n')
+
+    print('File convert done!')
 
 
+if __name__ == "__main__":
 
+    args = sys.argv
+    if len(args) != 3:
+        print("Please input openfile_name and savefile_name\n")
+        exit(0)
+    else:
+
+        main(args[1], args[2])
 
 
