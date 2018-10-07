efeeb094da3ac4a555a9b86ede8f9eeb1157cb93
xb2342996 <536523401@qq.com>
2018-10-07 18:19:59 +1100

    weight

--- code/data_clean.py
+++ code/data_clean.py
@@ -1,6 +1,7 @@
 import numpy as np
 import sys
 import os
+import collections
 
 
 def main(input, output):
@@ -8,32 +9,39 @@ def main(input, output):
     group_list = []
     data_dict = {}
     cord_list = []
-    count = 0
+    tag_list = []
+    count_dict = {}
+    count_list = []
 
     with open(input, 'r') as f:
         for l in f.readlines():
+
             tag, _, rssi = l.strip('\n').split(' ')
-            if tag != '-----End' and count == 1:
+            if tag != '-----End':
+                tag_list.append(tag)
                 if tag not in data_dict:
                     data_dict[tag] = [int(rssi)]
                 else:
                     data_dict[tag].append(int(rssi))
             elif tag == '-----End':
-                count += 1
-                if count == 2:
-                    _, position = rssi.split('-----')
-                    x, y = position.split(',')
-                    cord_list.append((x, y))
-                    group_list.append(data_dict)
-                    data_dict = {}
-                    count = 0
+                for i in set(tag_list):
+                    count_dict[i] = tag_list.count(i)
 
-    print(group_list)
-
+                # print(count_dict)
+                count_list.append(count_dict)
+                # print(collections.Counter(tag_list))
+                _, position = rssi.split('-----')
+                x, y = position.split(',')
+                cord_list.append((x, y))
+                group_list.append(data_dict)
+                data_dict = {}
+                tag_list = []
+                count_dict = {}
 
 
     for i in range(len(group_list)):
         for key, value in group_list[i].items():
+            # print(value)
             group_list[i][key] = np.mean(value)
 
         # print(group_list[i])
@@ -43,27 +51,33 @@ def main(input, output):
         os.remove(output)
 
     with open(output, 'a') as f:
-        f.write('%d,%d,%d,%d,%d,%s,%s\n' % (1,2,3,4,5,'x','y'))
+        f.write('%d,%d,%d,%d,%d,%s,%s,%s,%s,%s,%s,%s\n' % (1,2,3,4,5,'x','y','w1','w2','w3','w4','w5'))
 
         for i in range(len(group_list)):
+            sum = 0
             for j in range(1, 6):
                 if str(j) not in group_list[i]:
                     f.write('%.2f,' % 0)
                 else:
                     f.write('%.2f,' % group_list[i][str(j)])
-            f.write('%s,%s\n' % (cord_list[i][0], cord_list[i][1]))
-
-
+            f.write('%s,%s,' % (cord_list[i][0], cord_list[i][1]))
+            for s in range(1, 6):
+                sum += count_list[i][str(s)]
+            for k in range(1, 5):
+                f.write('%.2f,' % (count_list[i][str(k)]/sum))
+            f.write('%.2f' % (count_list[i]['5']/sum))
+            f.write('\n')
     print('File convert done!')
 
 
 if __name__ == "__main__":
 
-    args = sys.argv
-    if len(args) != 3:
-        print("Please input openfile_name and savefile_name\n")
-        exit(0)
-    else:
-        main(args[1], args[2])
+    # args = sys.argv
+    # if len(args) != 3:
+    #     print("Please input openfile_name and savefile_name\n")
+    #     exit(0)
+    # else:
+    #     main(args[1], args[2])
+    main('../data01/record03.txt', 'update_clean.txt')
 
 
