7d4de5a2f20d32b207b5ed0eb3cc2e7bc33bf528
Erik Zhou <erikxiangzhou@gmail.com>
2018-10-07 20:44:00 +1100

    tidy up folder and update predict.py

--- code/predict.py
+++ code/predict.py
@@ -9,66 +9,62 @@ import os
 import numpy as np
 import math
 
-#cwd = os.getcwd()
-#os.chdir("/iot")
-#print(cwd)
 
-#v = [-62.00 ,-84.33 ,-70.00 ,-64.75 ,-59.75] #5 3.2
-#v = [-59.50, -76.50, -57.50, -70.75, -62.33] # 5.7 5
-#v = [-64.75 ,0 ,-58.25 ,0 ,-65.50] #19 3.1]
-#v = [-69.33 ,-75.00 ,-54.50 ,-87.33 ,-71.00] #21 1.4]
-#v = [-62.25, 0, -66.00, 0, -59.75]# 7.5 3.3
-#v = [-62.25, -78.67, -66.00, -85.00, -59.75]# 7.5 3.3
-#v =[-63.50,-78.00,-63.00,-85.50,-59.50] #7.5,3.3]
-#v = [1,1,1,1,1]
-#v = [-82.25,-78.00, -71.25, -75.67, -61.33] #39,     -2
+os.chdir("/Users/erikzhou/Desktop/git_prac/iot_project/data02")
+cwd = os.getcwd()
+print(cwd)
 
+#v0 = [-71.20,-64.33,-81.67,-58.60,-69.50,7.3,2.5,0.22,0.20,0.13,0.22,0.2]
+#v0 =[-64.33,-65.83,-73.50,-60.00,-58.17,19.5,2.5,0.21,0.21,0.17,0.21,0.2]
+#v0 = [-63.40,-64.30,-72.60,-56.40,-67.50,13,2.7,0.20,0.20,0.20,0.20,0.2]
+#v0=[-59.40,-79.50,-68.25,-63.30,-73.80,2.3,2,0.21,0.21,0.17,0.21,0.2]
+#v0=[-71.60,-62.00,-85.11,-61.80,-65.60,24.2,4.4,0.20,0.20,0.18,0.20,0.2]
 
+#v0=[-66.90,-80.00,-77.50,-67.00,-59.80,11.1,3.3,0.20,0.20,0.20,0.20,0.20]
+#v0=[-70.50,-74.40,-79.00,-64.10,-48.90,14.5,2.8,0.20,0.20,0.20,0.20,0.20]
+#v0=[-72.70,-69.60,-74.50,-63.90,-58.90,14.5,4,0.20,0.20,0.20,0.20,0.20]
 
-
-#-83.50,-80.00,-77.50,-76.00,-63.00,39,-2
-#-84.50,-73.00,-83.00,0.00,0.00,39,-12.5
-#v= [-68.00,-70.50,-52.50,-87.00,-70.50] #21,1.4
-#-64.50,-75.00,-58.50,-88.00,-64.00,19,3.1
-#-63.50,-78.00,-63.00,-85.50,-59.50,7.5,3.3
-v= [0.00,-79.50,-68.00,-86.00,-68.50] #3.7,3.1
 
 #v = list(map(lambda x: 10**(x/100),v) )
+v = v0[:5]        #sample vector 
+origin = v0[5:7]    #origin coordinates
+w = v0[7:]          #the weight of each base station reading
+
 
 nearest ={};
 coordinate_v = {}
-weight_c = [1.2,0.8,0.3,0.2,0.1]
-weight = [0,0,0,0,0]
-#assign weight to v
-index ={}
-i = 0
-#weight = {index:value}
-for e in v:
-    index[i] = e
-    i += 1
+
+#assign weight according to weight_c
+def add_weight(weight_c,v):
+    #assign weight to v
+    index ={}
+    i = 0
+    weight = [0,0,0,0,0]
+    #weight = {index:value}
+    for e in v:
+        index[i] = e
+        i += 1
+        
+    value_order = sorted(index.items(), key=lambda kv: kv[1], reverse = True)
+    #print(value_order)
     
-value_order = sorted(index.items(), key=lambda kv: kv[1], reverse = True)
-#print(value_order)
-
-start =0
-for e in value_order:
-    (i, val) = e
-    weight[i] =weight_c[start]
-    start += 1
+    start =0
+    for e in value_order:
+        (i, val) = e
+        weight[i] =weight_c[start]
+        start += 1
+        
+    #print(weight)
     
-#print(weight)
-
-#assign weight element wise to v according to weight
-v = [round(a * b,1) for a, b in zip(weight, v)]
-
-
-
+    #assign weight element wise to v according to weight
+    v = [round(a * b,1) for a, b in zip(weight, v)]
+    return weight
 
 
 #for a given test point reading predict x,y output
 def dot_product(v):
     #read from test.txt file line by line:x1 x2 x3 x4 x5 x y
-    f = open('new1.txt', 'r')
+    f = open('clean_record.txt', 'r')
     for line in f.readlines():#reads the whole file into memory and returns its contents as a list of its lines.
         #assme input for each line would be x1 x2 x3 x4 x5 x y
         v1 = line.rstrip().split(',')
@@ -76,15 +72,23 @@ def dot_product(v):
         v1 = list(map(lambda x: float(x),v1))     
         
         #each line would be x1 x2 x3 x4 x5 x y find vector and coordinates
-        coordinate = v1[-2:]
-        vector = v1[:-2]
+        coordinate = v1[5:7]
+        vector = v1[:5]
+        global w
+        global coordinate_v
+        
         
         #assign weight to vector
-        vector = [round(a * b,1) for a, b in zip(weight, vector)]
-        
+        weight_c = [0.8,0.8,0.2,0.2,0]
+
         #print(vector)
        # vector = list(map(lambda x: 10**(x/100),vector) )
-        vd = [a - b for a, b in zip(vector, v)]
+           
+        vd = [round(a - b,1) for a, b in zip(vector, v)]
+        #print("before weight:" , vd)
+        vd = [round(a * b,1) for a, b in zip(w, vd)]    #apply count weigth to the difference
+        vd = [round(a * b,1) for a, b in zip(add_weight(weight_c,v),vd)]
+        #print("after weight:" , vd)
 
         #record the the dot product value and the coordinates in the dict
         # (x,y):dot_val
@@ -94,19 +98,47 @@ def dot_product(v):
     f.close
     return nearest
 
+#given 3 coordinates find the centre of the triangle output a coordinate
+#input : [(x,y)...]
+def find_gravity(near_xs,near_ys):
+    u1 = round(np.mean(near_xs),1)
+    u2 = round(np.mean(near_ys),1)
+    
+    return u1, u2
+    
+    
+
 
 #find the top 3 smallest dot product coordinates
 #we have dot_val:[x,y]
 nearest = dot_product(v)
 order_nearest = sorted(nearest.items(), key=lambda kv: kv[1]) #[((5.7, 5.0), 0.0),...]
 
-print("Original vectore is :      ",v)
+#print(add_weight([0.8,0.8,0.2,0.2,0],[-1,-2,-3,-4,-5]))
+
+
 print("Dot_val:    Coordinates:")
+print("=======================================")
+near_xs = []
+near_ys = []
 for key in order_nearest:
     ((x,y),dot_val) = key
+    
+    near_xs.append(x)
+    near_ys.append(y)
 #    #c = list(map(lambda x: round(math.log(x,10)*100,1),(nearest[key])))
     print("{:10.4f}".format(round(dot_val,4)), "    ","({:4.1f},{:4.1f})".format(x,y), coordinate_v[(x,y)])
    
-    
+
+print()
+print("Original vector:      ")
+print("=======================================")
+print("{:10.4f}".format(round(0,4)), "    ","({:4.1f},{:4.1f})".format(origin[0],origin[1]), v)
+
+#find the centre of the nearest 3 points as the predicted location of node x
+u1, u2 = find_gravity(near_xs,near_ys)
+print("The Prediced    :({},{})".format(u1,u2))
+err = round(math.sqrt((u1-origin[0])**2 + (u2-origin[1])**2),1)
+print("The error is    :", err)
     
 
